4b02b3093f097b1490eb1251fd6513d67dbe964c
matthewahiles <matthewahiles@gmail.com>
2021-11-30 17:29:40 -0600

    first day solution

---
+++ src/days/1/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const input = fs.readFileSync('./input.txt', 'ascii')
+const R = require('ramda')
+
+const solutionOne = R.pipe(
+  R.reduce(([previous, count], val) => val > previous ? [val, count + 1] : [val, count], [0, 0]),
+  R.last,
+  n => n - 1
+)
+const solutionTwo = R.pipe(
+  R.aperture(3),
+  R.map(R.sum), solutionOne
+)
+
+const parseInput = R.pipe(
+  R.split('\n'),
+  R.map(Number)
+)
+
+R.pipe(parseInput, solutionTwo, console.log)(input)
